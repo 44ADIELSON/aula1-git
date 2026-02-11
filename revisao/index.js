0acc86573f287cc037c1dbca3559365401ce5383
user2026 <email_user2026@gmail.com>
2026-02-11 13:26:32 -0300

    alteraçoes no arquivo

--- revisao/index.js
+++ revisao/index.js
@@ -3,7 +3,10 @@ let segunNumer = prompt(`Digite um número`)
 
 function impar (primeNumer,segunNumer){
     if(primeNumer / 2 == 0){
-        
+        alert(`${primeNumer} é um numero par`)
+    }
+    else {
+        alert(`${segunNumer} é um número impar`)
     }
 
 }
