50a5bef62458777fd5bbe42af804043f67b9f67a
user2026 <email_user2026@gmail.com>
2026-02-11 13:20:36 -0300

    arquivos

---
+++ revisao/index.js
@@ -0,0 +1,9 @@
+let primeNumer = prompt(`Digite um número`)
+let segunNumer = prompt(`Digite um número`)
+
+function impar (primeNumer,segunNumer){
+    if(primeNumer / 2 == 0){
+        
+    }
+
+}
